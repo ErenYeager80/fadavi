fcd38bb34241883b9b26c5251de6ccda4972d0a5
unknown <amir_jabini2000@yahoo.com>
2023-04-01 12:43:47 +0430

    first upload

---
+++ fadavi/js/main.js
@@ -0,0 +1,21 @@
+// const orderPlus = document.getElementById('order_plus');
+// const orderMinus = document.getElementById('order_minus');
+// const orderChange = document.getElementById('order_change');
+// let i = 0;
+
+
+
+// orderPlus.addEventListener('click',() => {
+//     orderChange.value = ++i;
+// })
+
+// orderMinus.addEventListener('click', () =>  {
+//     orderChange.value = --i;
+// })
+
+const btnNavbar = document.getElementById('btnNavbar');
+const sidebar = document.getElementById('sidebar');
+btnNavbar.addEventListener('click', () => {
+    sidebar.classList.toggle('sidebar-show');
+    sidebar.classList.toggle('sidebar-hide');
+})
